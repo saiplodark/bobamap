63e66132952c0f486af0b7f951c216a79ba660f8
Cory iMac27 MD17 <cory@Cory-iMac27-MD17.local>
2020-05-18 16:32:03 -0700

    index done

---
+++ server/index.js
@@ -0,0 +1,47 @@
+require ('dotenv').config()
+const 
+express = require('express'),
+session = require ('express-session'),
+massive = require('massive'),
+path = require('path'),
+{SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
+app = express();
+
+app.use(express.static(`${__dirname}/../build`));
+app.use(express.json());
+app.use(session({
+    secret:SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        maxAge:1000*60*60*24*14
+    }
+}))
+
+massive({
+    connectionString:CONNECTION_STRING,
+    ssl:{ rejectUnauthorized:false}
+}).then(db=>{
+    app.set('db', db)
+    console.log('db testing connected')
+}).catch(err=>console.log('db can not connect'))
+
+// //AUTH//
+// app.post('/api/register', register)
+// app.post('/api/login', login)
+// app.post('/api/logout', logout)
+// app.get('/auth/user_session', userSession)
+
+// //Stores//
+// app.get('/api/stores', getStores)
+// app.post('/api/addstores', addStores)
+// app.put('/api/editstores/:id', editStores)
+// app.delete('/api/deletestores/:id', deleteStores)
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+app.listen(SERVER_PORT, ()=>{
+    console.log(`Server connect  to port ${SERVER_PORT}`)
+})
